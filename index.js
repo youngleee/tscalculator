569924ab7e6b9702a8a13757a72ea9c3478842b0
Young Lee <youngleeee@outlook.com>
2024-06-22 22:46:10 +0200

    added operators and strings

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello bingus");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// we need readline sync, includes types that are required (npm i --save @types/readline-sync)
+var readline_sync_1 = require("readline-sync"); // semi- conols optional
+function main() {
+    var firstStr = (0, readline_sync_1.question)('Enter first number: \n');
+    var operator = (0, readline_sync_1.question)('Enter operator: \n');
+    var secondStr = (0, readline_sync_1.question)('Enter second number: \n');
+    console.log(firstStr, operator, secondStr);
+}
+main();
--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-// we need readline sync 
+// we need readline sync, includes types that are required (npm i --save @types/readline-sync)
+import {question} from 'readline-sync'; // semi- conols optional
+
+function main(): void {
+
+    const firstStr: string = question('Enter first number: \n');
+    const operator: string = question('Enter operator: \n')
+    const secondStr: string = question('Enter second number: \n')
+    // need to convert string to numbers for calculation
+    console.log(firstStr,operator,secondStr);
+    // need to check for valid inputs
+}
+
+main();
